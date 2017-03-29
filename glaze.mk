b9d769dab17cda9808581a4b99c30246ef7e315a
NoelMacwan <yulespencer@gmail.com>
2017-03-29 17:42:39 +0530

    k3gxx: rebrand to GlazeOS

--- glaze.mk
+++ glaze.mk
@@ -1,15 +1,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := k3gxx
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Glaze stuff.
+$(call inherit-product, vendor/glaze/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/k3gxx/device.mk)
 $(call inherit-product, device/samsung/k3gxx/system_prop.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_k3gxx
+PRODUCT_NAME := glaze_k3gxx
 PRODUCT_DEVICE := k3gxx
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-G900H
